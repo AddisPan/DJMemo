aa05009fb622435d8d268ae52f956bcbb192e366
AddisPan <87139661+AddisPan@users.noreply.github.com>
2026-03-06 14:38:50 +0800

    Update MainActivity.java
    Modify by website

--- app/src/main/java/com/example/commoneydjdjmemo/MainActivity.java
+++ app/src/main/java/com/example/commoneydjdjmemo/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         //這是Master&Branch
         //修好 Bug
-        //This is modify by GitHub website
+        //This is modify by website
         // 初始化時載入登入頁面
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
