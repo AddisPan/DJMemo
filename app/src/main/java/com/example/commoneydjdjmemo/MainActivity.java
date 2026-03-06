aac63a04f2ffc753fedcf1e504ecc0abf401b03f
addispan <addispan@moneydj.com>
2026-03-06 14:18:29 +0800

    feat: master modify MainActivity

--- app/src/main/java/com/example/commoneydjdjmemo/MainActivity.java
+++ app/src/main/java/com/example/commoneydjdjmemo/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //這是Master & Branch 合力完成的動作
+        //這是Master
         // Modified By GitHub website
         // 初始化時載入登入頁面
         if (savedInstanceState == null) {
