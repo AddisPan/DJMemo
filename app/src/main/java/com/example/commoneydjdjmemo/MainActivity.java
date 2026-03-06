6547697bc3c4805bc0efcbcaaf7db5269f35ca7f
addispan <addispan@moneydj.com>
2026-03-06 10:29:02 +0800

    feat: modify complete!

--- app/src/main/java/com/example/commoneydjdjmemo/MainActivity.java
+++ app/src/main/java/com/example/commoneydjdjmemo/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //這是Master & Branch 合力完成的
+        //這是Master & Branch 合力完成的動作
         // 初始化時載入登入頁面
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
